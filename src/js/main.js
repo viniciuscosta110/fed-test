0403c76521bb7b22899ccebba02c21802cf988ad
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2023-07-06 18:43:54 -0300

    fix: add missing plus

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
